e7215f001b7e6820da113142e2f67f992f01aeef
BorisKiselev <hugh.dancy@icloud.com>
2022-09-02 20:08:55 +0700

    Add miniplayer

---
+++ HW17/View/Modules/MiniPlayer/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  HW17
+//
+//  Created by Борис Киселев on 02.09.2022.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterial))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
+}
