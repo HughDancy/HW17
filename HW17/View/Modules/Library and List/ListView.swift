94a02784bac054e285238db57a3e361ea96cfa18
BorisKiselev <hugh.dancy@icloud.com>
2022-09-02 20:06:35 +0700

    Add LibraryView and ListView

---
+++ HW17/View/Modules/Library and List/ListView.swift
@@ -0,0 +1,54 @@
+//
+//  ListView.swift
+//  HW17
+//
+//  Created by Борис Киселев on 02.09.2022.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @State private var multisection = Set<UUID>()
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        
+        
+        List(selection: $multisection) {
+            ForEach(categories, id: \.self) { element in
+                HStack {
+                    Image(systemName: element.icon)
+                        .foregroundColor(.pink)
+                    Text(element.category)
+                }
+            } .onMove(perform: move)
+            
+            
+        }
+        .environment(\.editMode, Binding.constant(EditMode.active))
+        .listStyle(.plain)
+        .navigationTitle("Медиатека")
+        .accentColor(.pink)
+        
+        .toolbar {
+            
+            Button("Готово") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }.navigationBarBackButtonHidden(true)
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        categories.move(fromOffsets: source, toOffset: destination)
+        
+    }
+}
+
+
+struct MediaView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
+
