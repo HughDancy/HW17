8efefff132375e24d6bd185dd1602b06e9dad302
BorisKiselev <hugh.dancy@icloud.com>
2022-09-02 19:46:01 +0700

    Initial Commit

---
+++ HW17/HW17App.swift
@@ -0,0 +1,17 @@
+//
+//  HW17App.swift
+//  HW17
+//
+//  Created by Борис Киселев on 02.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
