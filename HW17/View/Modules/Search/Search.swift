0ac1bbab4ea30393403f35c31cf6f6d4f67087b2
BorisKiselev <hugh.dancy@icloud.com>
2022-09-15 23:03:25 +0700

    Working search (with artist, album and song name

--- HW17/View/Modules/Search/Search.swift
+++ HW17/View/Modules/Search/Search.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct Search: View {
+    @State private var multisection = Set<UUID>()
     
     @State private var search = ""
     
     var columns = Array(repeating: GridItem(.flexible(), spacing: 20), count: 2)
+    var secondColumns = Array(repeating: GridItem(.flexible(), spacing: 20), count: 1)
+    
     @ObservedObject var someAlbums = Albums()
+    @ObservedObject var someSongs = Songs()
+    
   
     var filtredAlbums: [Album] {
         if search == "" { return someAlbums.albums }
@@ -21,42 +26,33 @@ struct Search: View {
         }
     }
     
-    
+    var filtredSongs: [Song] {
+        if search == "" { return someSongs.songs}
+        return someSongs.songs.filter {
+            $0.name.lowercased().contains(search.lowercased()) || $0.artist.lowercased().contains(search.lowercased()) || $0.album.lowercased().contains(search.lowercased())
+        }
+    }
+ 
+     
     
     var body: some View {
         NavigationView {
            
             ScrollView(showsIndicators: false) {
                 
-                VStack(spacing: 18) {
-//                    HStack {
-//                        
-//                        Text("Поиск")
-//                            .font(.largeTitle)
-//                            .fontWeight(.heavy)
-//                            .foregroundColor(.primary)
-//                            .padding(.leading, 10)
-//                        
-//                        Spacer(minLength: 0)
-//                    }
-//                    
-//                    HStack(spacing: 15) {
-//                        
-//                        Image(systemName: "magnifyingglass")
-//                            .foregroundColor(.primary)
-//                            .padding(.horizontal, 10)
-//                        
-//                        TextField("Поиск", text: $search)
-//                    }
-//                    
-//                    .padding(.vertical, 10)
-//                    .padding(.horizontal)
-//                    .background(Color.primary.opacity(0.06))
-//                    .cornerRadius(15)
-                    
+               
+                
+                VStack(alignment: .leading, spacing: 18) {
+                    Text("Альбомы")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 10)
+                  
                     LazyVGrid(columns: columns , spacing: 20){
                         ForEach(filtredAlbums, id: \.self) { index in
-                            ZStack {
+                    
+                            VStack {
                                 Image(index.image)
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
@@ -64,21 +60,64 @@ struct Search: View {
                                     .cornerRadius(15)
                                 
                                 Text(index.name)
-                                    .foregroundColor(.clear)
+                                    .foregroundColor(.black)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .multilineTextAlignment(.center)
                                 Text(index.artist)
-                                    .foregroundColor(.clear)
-                                
+                                    .foregroundColor(.black)
+                                    .font(.caption)
                             }
-                            
                         }
+                      
+                        
                         
                     }
                     .navigationTitle("Поиск")
                    
                     .searchable(text: $search, prompt: "Найди свою музыку")
+                    .padding(.bottom, 20)
                
+                    
+                    Text("Песни")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 20)
+                    
+                    LazyVGrid(columns: secondColumns, alignment: .leading, spacing: 20) {
+                        ForEach(filtredSongs, id: \.self) { song in
+                            HStack {
+                                Image(song.image)
+                                    .resizable()
+                                    .frame(width: 50, height: 50, alignment: .leading)
+                                    .cornerRadius(10)
+                                
+                                Text(song.artist)
+                                    .font(.caption)
+                                    .foregroundColor(.black)
+                                
+                                Text(song.name)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(.leading)
+                                    .foregroundColor(.black)
+                                
+                                Text(song.album)
+                                    .font(.caption)
+                                    .foregroundColor(.clear)
+                                
+                                
+                            }
+                          
+                        }
+                    }
+                    .padding(.leading, 15)
+                    .padding(.bottom, 80)
+                 
                 }
-                .padding(.bottom, 90)
+               
             }
             
         }
